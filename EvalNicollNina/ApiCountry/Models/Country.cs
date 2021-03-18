ab4da5a0eddb38a861cade27d827a940fc2cc500
Karla Nicoll Nina Muñoz <a2016118253@estudiantes.upsa.edu.bo>
2021-03-18 18:46:40 -0400

    Modelo incorporado

---
+++ EvalNicollNina/ApiCountry/Models/Country.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApiCountry.Models
+{
+    public class Country
+    {
+        [Key]
+        public String Name { get; set; }
+        [Required]
+        public String Capital { get; set; }
+        public int Population { get; set; }
+        public List<double> Latlng { get; set; }
+        public List<String> Timezones { get; set; }
+        public List<Currency> Currencies { get; set; }
+        public String flag { get; set; }
+    }
+}
---
+++ EvalNicollNina/ApiCountry/Models/Currency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApiCountry.Models
+{
+    public class Currency
+    {
+        [Key]
+        public String Code { get; set; }
+        [Required]
+        public String Name { get; set; }
+        [Required]
+        [StringLength(5,MinimumLength =1,ErrorMessage ="Ingrese entre 1 a 5 caracteres")]
+        public String Symbol { get; set; }
+
+    }
+}
